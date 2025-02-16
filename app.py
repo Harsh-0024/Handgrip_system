82553c1d96f111720b28f0d2262ebd3b89ce83ea
Harsh 24 <harsh242042004@gmail.com>
2025-02-16 12:29:05 +0530

    improving code functionality so that it returns to program detail

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, request, render_template
 app = Flask(__name__)
 
-power_strength = [[12, 1,], [11, 1], [10, 3], [9, 5], [8, 7], [7, 9], [6, 11]]
-endurance_isometric = [[3, 64], [5, 54], [7, 45], [9, 36], [11, 24]]
+power_strength = [[12, 1, 3], [11, 1, 3], [10, 3, 3], [9, 5, 3], [8, 7, 5], [7, 9, 5], [6, 11, 5]]
+endurance_isometric = [[3, 64, 5], [5, 54, 5], [7, 45, 5], [9, 36, 5], [11, 24, 5]]
 
 
 @app.route('/')
@@ -15,11 +15,9 @@ def program():
     mode = request.args.get('mode')
     if mode == 'power':
         return_list = power_strength
-        rest = 3
     else:
         return_list = endurance_isometric
-        rest = 5
-    return render_template('program_detail.html', list=return_list, rest=rest)
+    return render_template('program_detail.html', list=return_list, mode=mode)
 
 
 @app.route('/workout')
@@ -27,7 +25,8 @@ def workout():
     timex = request.args.get('time')
     reps = request.args.get('reps')
     rest = request.args.get('rest')
-    return render_template('workout.html', hold=timex, rest=rest, count=reps)
+    modex = request.args.get('mode')
+    return render_template('workout.html', hold=timex, rest=rest, count=reps, mode=modex)
 
 
 if __name__ == '__main__':
