d6baeb04403fc9145bd20adc8147ec132580e0ba
Harsh 24 <harsh242042004@gmail.com>
2025-02-16 12:39:32 +0530

    improving code, doubled endurance reps

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 
 power_strength = [[12, 1, 3], [11, 1, 3], [10, 3, 3], [9, 5, 3], [8, 7, 5], [7, 9, 5], [6, 11, 5]]
-endurance_isometric = [[3, 64, 5], [5, 54, 5], [7, 45, 5], [9, 36, 5], [11, 24, 5]]
+endurance_isometric = [[3, 64, 3], [5, 54, 3], [7, 45, 3], [9, 36, 3], [11, 24, 3]]
 
 
 @app.route('/')
@@ -23,9 +23,9 @@ def program():
 @app.route('/workout')
 def workout():
     timex = request.args.get('time')
-    reps = request.args.get('reps')
     rest = request.args.get('rest')
     modex = request.args.get('mode')
+    reps = request.args.get('reps') if modex == 'power' else int(request.args.get('reps'))*2
     return render_template('workout.html', hold=timex, rest=rest, count=reps, mode=modex)
 
 
