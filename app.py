716f3e931df18f95041f38f6d6a33c66c2673071
Harsh 24 <harsh242042004@gmail.com>
2025-02-16 23:48:31 +0530

    time invested to be present in int

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ from flask import Flask, request, render_template, send_from_directory
 
 app = Flask(__name__)
 
-power_strength = [[12, 1, 3], [11, 1, 3], [10, 3, 3], [9, 5, 3], [8, 7, 5], [7, 9, 5], [6, 11, 5]]
-endurance_isometric = [[3, 64, 3], [5, 54, 3], [7, 45, 3], [9, 36, 3], [11, 24, 3]]
+power_strength = [[12, 1, 3, 1], [11, 1, 3, 2], [10, 3, 3, 6], [9, 5, 3, 7],
+                  [8, 7, 5, 10], [7, 9, 5, 10], [6, 11, 5, 10]]
+endurance_isometric = [[3, 64, 3, 13], [5, 54, 3, 19], [7, 45, 3, 22], [9, 36, 3, 23], [11, 24, 3, 20]]
 
 
 @app.route('/static/<path:filename>')
@@ -33,8 +34,10 @@ def workout():
     timex = int(request.args.get('time'))
     rest = int(request.args.get('rest'))
     modex = request.args.get('mode')
+    time_invested = request.args.get('time_invested')
     reps = int(request.args.get('reps')) if modex == 'power' else int(request.args.get('reps'))*2
-    return render_template('workout.html', hold=timex, rest=rest, count=reps, mode=modex)
+    return render_template('workout.html', hold=timex, rest=rest, count=reps, mode=modex,
+                           time_invested=time_invested)
 
 
 if __name__ == '__main__':
